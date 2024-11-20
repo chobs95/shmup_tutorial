bc33b23532faa557a2824c76d4d695b12ccd6b0b
chobs95 <charlieroberts201@hotmail.co.uk>
2024-11-20 12:29:16 +0000

    recent changes

--- schmup_tutorial_game/laser.gd
+++ schmup_tutorial_game/laser.gd
@@ -3,4 +3,5 @@ extends Node2D
 @export var laser_speed: float = 900	
 
 func _process(delta):
+	z_index = 1
 	position.y -= laser_speed * delta
--- schmup_tutorial_game/player.gd
+++ schmup_tutorial_game/player.gd
@@ -3,10 +3,13 @@ extends Node2D
 @export 	var laser_tscn: PackedScene
 
 func _process(delta):
+	z_index = 10
 	var mouse_pos = get_global_mouse_position()
 	position.x = mouse_pos.x
 	if Input.is_action_just_pressed("Fire"):
 		print("firing laser")
 		var new_laser = laser_tscn.instantiate()
-		add_child(new_laser)
+		self.add_sibling(new_laser)
+	
+		new_laser.position = self.position 
 		
